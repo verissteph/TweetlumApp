cef1cb7a422fdd2c6dcec0a650a60a9f3dcb5527
Pinheiro <stephanie.v.pinheiro@accenture.com>
2020-12-08 21:53:58 -0300

    incluindo menu e botao voltar seguindo a apostila

--- app/src/main/java/caelum/com/twittelumapp/TweetActivity.kt
+++ app/src/main/java/caelum/com/twittelumapp/TweetActivity.kt
@@ -2,17 +2,32 @@ package caelum.com.twittelumapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.EditText
 import android.widget.Toast
 
 class TweetActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_tweet)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+          }
 
-        val botaoPublicar = findViewById<Button>(R.id.publicar_tweet)
-        botaoPublicar.setOnClickListener{ publicaTweet() }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.tweet_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    when(item?.itemId){
+        R.id.tweet_menu_cadastrar ->{
+            publicaTweet()
+            finish()
+        }
+        android.R.id.home -> finish()
+    }
+        return false
     }
 
     fun publicaTweet(){
