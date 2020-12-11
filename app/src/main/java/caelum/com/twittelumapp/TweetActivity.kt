bf4dab0ea3c726a14411d8e5bdb5679bccbc3bb4
Your Name <you@example.com>
2020-12-11 15:38:56 -0300

    capturando e retornando foto

--- app/src/main/java/caelum/com/twittelumapp/TweetActivity.kt
+++ app/src/main/java/caelum/com/twittelumapp/TweetActivity.kt
@@ -1,51 +1,78 @@
 package caelum.com.twittelumapp
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.core.content.FileProvider
+import androidx.core.content.FileProvider.getUriForFile
 import androidx.lifecycle.ViewModelProvider
 import caelum.com.twittelumapp.bancodedados.TwittelumDatabase
+import caelum.com.twittelumapp.databinding.ActivityTweetBinding
 import caelum.com.twittelumapp.modelo.Tweet
 import caelum.com.twittelumapp.vm.TweetViewModel
 import caelum.com.twittelumapp.vm.ViewModelFactory
+import com.google.android.material.transition.FadeProvider
+import java.io.File
 
 class TweetActivity : AppCompatActivity() {
-    private val viewModel: TweetViewModel by lazy{
-        ViewModelProvider(this,ViewModelFactory).get(TweetViewModel::class.java)
+    private var localFoto: String? = null
+    private val viewModel: TweetViewModel by lazy {
+        ViewModelProvider(this, ViewModelFactory).get(TweetViewModel::class.java)
     }
+    private lateinit var binding: ActivityTweetBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tweet)
+        binding = ActivityTweetBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-          }
+    }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.tweet_menu,menu)
+        menuInflater.inflate(R.menu.tweet_menu, menu)
         return true
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-    when(item?.itemId){
-        R.id.tweet_menu_cadastrar ->{
-            publicaTweet()
-            finish()
+    override fun onResume() {
+        super.onResume()
+        if (localFoto != null) {
+            carregaFoto()
         }
-        R.id.menu_camera -> {
-            val abrirCamera = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            startActivity(abrirCamera)
-            true
-        }
-        android.R.id.home -> finish()
     }
+
+    private fun carregaFoto() {
+        val bitmap = BitmapFactory.decodeFile(localFoto)
+        val bm = Bitmap.createScaledBitmap(bitmap, 300, 300, true)
+        binding.tweetFoto.setImageBitmap(bm)
+        binding.tweetFoto.scaleType = ImageView.ScaleType.FIT_XY
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId) {
+            R.id.tweet_menu_cadastrar -> {
+                publicaTweet()
+                finish()
+            }
+            R.id.menu_camera -> {
+                tiraFoto()
+                true
+            }
+            android.R.id.home -> finish()
+        }
         return false
     }
 
-    private fun publicaTweet(){
+    private fun publicaTweet() {
         val campoDeMensagemDoTweet = findViewById<EditText>(R.id.conteudo_tweet)
         val mensagemDoTweet: String = campoDeMensagemDoTweet.text.toString()
         val tweet = Tweet(mensagemDoTweet)
@@ -53,4 +80,17 @@ class TweetActivity : AppCompatActivity() {
 //        Toast.makeText(this,"$tweet foi salvot",Toast.LENGTH_LONG).show()
     }
 
+    private fun tiraFoto() {
+        val abrirCamera = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        val caminhoFoto = defineLocalDaFoto()
+        abrirCamera.putExtra(MediaStore.EXTRA_OUTPUT, caminhoFoto)
+        startActivity(abrirCamera)
+    }
+
+    fun defineLocalDaFoto(): Uri? {
+        localFoto = "${getExternalFilesDir(Environment.DIRECTORY_PICTURES)}/${
+            System.currentTimeMillis()}.jpg"
+        val arquivo = File(localFoto)
+        return FileProvider.getUriForFile(this, "br.com.twittelumapp.fileprovider", arquivo)
+    }
 }
